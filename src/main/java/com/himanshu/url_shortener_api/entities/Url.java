3f48b062038a92638af1d0b8343bc4a194cbed1b
Himanshu <him2282001@gmail.com>
2024-07-19 00:04:27 +0530

    Add a DTO for handling requests, mapper for mapping data from DTO to entity and add annotations for missing getters and setters in URL class.

--- src/main/java/com/himanshu/url_shortener_api/entities/Url.java
+++ src/main/java/com/himanshu/url_shortener_api/entities/Url.java
@@ -7,12 +7,16 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.util.Date;
 
 @NoArgsConstructor
 @AllArgsConstructor
+@Data
+@Builder
 @Entity
 @Table(name = "url")
 public class Url {
