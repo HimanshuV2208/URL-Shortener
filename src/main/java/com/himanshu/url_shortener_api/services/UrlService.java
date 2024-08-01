63f89de8713e7da675fbca099dfb7b6809e4376c
Himanshu <him2282001@gmail.com>
2024-08-01 19:15:06 +0530

    Add logger to Service class

--- src/main/java/com/himanshu/url_shortener_api/services/UrlService.java
+++ src/main/java/com/himanshu/url_shortener_api/services/UrlService.java
@@ -6,6 +6,8 @@ import com.himanshu.url_shortener_api.mappers.UrlMapper;
 import com.himanshu.url_shortener_api.repositories.UrlRepository;
 import com.himanshu.url_shortener_api.utils.BaseConversionUtil;
 import jakarta.persistence.EntityNotFoundException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -13,6 +15,8 @@ import java.util.InvalidPropertiesFormatException;
 
 @Service
 public class UrlService {
+
+    private static final Logger logger = LoggerFactory.getLogger(UrlService.class);
     private final UrlRepository urlRepository;
     private final UrlMapper urlMapper;
 
@@ -22,20 +26,31 @@ public class UrlService {
     }
 
     public String convertToShortUrl(UrlLongRequestDTO urlLongRequestDTO) {
+        logger.debug(String.format("Service : DTO received -> %s", urlLongRequestDTO));
         Url mappedUrl = urlMapper.urlLongRequestToUrl(urlLongRequestDTO);
+        logger.debug(String.format("Service : Mapped to Entity -> %s", mappedUrl));
         Url urlSavedInDb = urlRepository.save(mappedUrl);
+        logger.debug(String.format("Service : Saved in DB -> %s", urlSavedInDb));
         String encodedUrl = BaseConversionUtil.IdToB62EncodedString(urlSavedInDb.getId());
+        logger.debug(String.format("Service : Corresponding encoded URL -> %s", encodedUrl));
         return encodedUrl;
     }
 
     public String getOriginalUrl(String shortUrl) throws InvalidPropertiesFormatException {
+        logger.debug(String.format("Service : Received short URL -> %s", shortUrl));
         Long id = BaseConversionUtil.B62EncodedStringToId(shortUrl);
+        logger.debug(String.format("Service : Received short URL -> %d", id));
         Url urlFromDb = urlRepository.findById(id)
                 .orElseThrow(() -> new EntityNotFoundException("No URL with short URL : " + shortUrl));
+        logger.debug(String.format("Service : Retrieved URL from DB -> %s", urlFromDb));
         boolean isExpired = urlFromDb.getExpiryDate() != null && urlFromDb.getExpiryDate().isBefore(LocalDateTime.now());
+        logger.debug(String.format("Service : ExpiryDate -> %s | CurrentDate -> %s | isExpired -> %b", urlFromDb.getExpiryDate(), LocalDateTime.now(), isExpired));
         if (isExpired) {
+            logger.debug("Service : Link Expired");
+            urlRepository.delete(urlFromDb);
             throw new EntityNotFoundException("This link has expired!");
         }
+        logger.debug(String.format("Service : Returned long URL -> %s", urlFromDb.getLongUrl()));
         return urlFromDb.getLongUrl();
     }
 
