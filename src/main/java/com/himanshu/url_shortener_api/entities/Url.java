291db971ccd5989f99d3464a1f865231cdaa00e1
Himanshu <him2282001@gmail.com>
2024-08-02 15:55:20 +0530

    Modify datasource

--- src/main/java/com/himanshu/url_shortener_api/entities/Url.java
+++ src/main/java/com/himanshu/url_shortener_api/entities/Url.java
@@ -21,7 +21,7 @@ import java.time.LocalDateTime;
 @Table(name = "url")
 public class Url {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column(nullable = false)
     private String longUrl;
